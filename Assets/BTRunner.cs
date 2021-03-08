3f1559926bbd87836a46bd039c4b38624f579402
Sejadis <sejadis-dev@gmx.de>
2021-03-08 13:58:57 +0100

    add sensor to scene

--- Assets/BTRunner.cs
+++ Assets/BTRunner.cs
@@ -2,19 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using AI.BT;
+using AI.BT.Sensors;
 using UnityEngine;
 
-public class BTRunner : MonoBehaviour
+public class BTRunner : MonoBehaviour, IBlackboardProvider
 {
     public BehaviorTree tree;
 
     public int frequency;
 
     private float nextActivation;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -26,4 +27,6 @@ public class BTRunner : MonoBehaviour
             nextActivation = Time.time + 1f / frequency * 60;
         }
     }
+
+    public Blackboard Blackboard => tree.Blackboard;
 }
