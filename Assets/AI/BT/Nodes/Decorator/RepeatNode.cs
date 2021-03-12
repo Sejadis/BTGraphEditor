894c0aa54fe4f08cf1fd94c09dc0345a76020b3f
Sejadis <sejadis-dev@gmx.de>
2021-03-12 14:36:52 +0100

    refactor repeat node to use accessor

--- Assets/AI/BT/Nodes/Decorator/RepeatNode.cs
+++ Assets/AI/BT/Nodes/Decorator/RepeatNode.cs
@@ -6,23 +6,29 @@ namespace AI.BT.Nodes.Decorator
     [Serializable]
     public class RepeatNode : DecoratorNode
     {
-        [Input] public int limit = 3;
-        private int maxRepeats = -1;
+        [Input, Option] public BlackboardAccessor<int> Repeats;
 
         public override ResultState Execute()
         {
+            if (!Repeats.IsSet() || !Repeats.TryGetValue(out var limit))
+            {
+                return CurrentState = ResultState.Failure;
+            }
+
             if (limit <= 0)
             {
                 //TODO handle 0 differently for unlimited?
                 return ResultState.Failure;
             }
-            for (int i = 0; i < limit; i++)
+
+            for (var i = 0; i < limit; i++)
             {
                 var state = children[0].Execute();
                 if (state == ResultState.Success)
                 {
                     continue;
                 }
+
                 return CurrentState = state;
             }
 
